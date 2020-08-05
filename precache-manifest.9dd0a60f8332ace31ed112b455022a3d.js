8ff02a96e7be6d514e16f044220e219f735fa00b
Yuriy Borys <iam618384@gmail.com>
2020-08-05 14:56:02 +0300

    Updates

---
+++ precache-manifest.9dd0a60f8332ace31ed112b455022a3d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ba60a7d7a2c291b358d792a0ee1f2a",
+    "url": "/steprotect/index.html"
+  },
+  {
+    "revision": "1fcd847465a8487aa672",
+    "url": "/steprotect/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "416d4813b3ae75fb1338",
+    "url": "/steprotect/static/css/main.3b215504.chunk.css"
+  },
+  {
+    "revision": "1fcd847465a8487aa672",
+    "url": "/steprotect/static/js/2.5bb97ed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/steprotect/static/js/2.5bb97ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416d4813b3ae75fb1338",
+    "url": "/steprotect/static/js/main.8c78f9b0.chunk.js"
+  },
+  {
+    "revision": "1883b139e2a60daef005",
+    "url": "/steprotect/static/js/runtime-main.a54dd7a4.js"
+  },
+  {
+    "revision": "16a978e12dc108806817877b600099e0",
+    "url": "/steprotect/static/media/footer-map-1-1.16a978e1.png"
+  },
+  {
+    "revision": "ce5cfc0a7614847fcad6bc175e8486b5",
+    "url": "/steprotect/static/media/logo_footer.ce5cfc0a.svg"
+  },
+  {
+    "revision": "b20353bcde1b99b2f75eab8261b3db8c",
+    "url": "/steprotect/static/media/logo_hr.b20353bc.svg"
+  },
+  {
+    "revision": "55fa8c4d2050612563aafe738d248d64",
+    "url": "/steprotect/static/media/logo_vr.55fa8c4d.svg"
+  },
+  {
+    "revision": "4b9ca08f8a65e4462ce2d62d73ff65aa",
+    "url": "/steprotect/static/media/slider-1-1.4b9ca08f.jpg"
+  },
+  {
+    "revision": "2e74f0c17a0188701be062f75f6a11f3",
+    "url": "/steprotect/static/media/slider-1-2.2e74f0c1.jpg"
+  },
+  {
+    "revision": "73655932af50932243ebad3be0aa95f4",
+    "url": "/steprotect/static/media/slider-1-3.73655932.jpg"
+  }
+]);
