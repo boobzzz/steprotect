76444478ed819454accb1cf845de6a3126f42d90
Yuriy Borys <iam618384@gmail.com>
2020-09-28 14:40:30 +0300

    Prepare to deploy

--- server/src/index.js
+++ server/src/index.js
@@ -5,23 +5,35 @@ import cors from 'cors';
 import * as MC from './controllers/MailController';
 import * as PC from './controllers/PostController';
 import './config/database';
+// import routes from './controllers/routes';
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use(express.json())
 app.use(cors())
 
-//-------------------------------Mailer routes----------------------------------
+app.use(express.json())
+
+// app.use('./controllers/routes', routes)
+
+// Mailer routes
 app.post('/call', MC.sendNewCall)
 app.post('/order', MC.sendNewOrder)
 
-//--------------------------------Blog routes-----------------------------------
+// Blog routes
 app.get('/posts', PC.indexPost)
 app.post('/posts', PC.createPost)
 app.get('/posts/:id', PC.readPost)
 app.put('/posts/:id', PC.updatePost)
 app.delete('/posts/:id', PC.deletePost)
+
+// Set static folder
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.info(`Server has started on ${PORT} port`))
